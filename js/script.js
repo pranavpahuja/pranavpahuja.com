457c43804f2f1ddc3d668439ce997d67c8711c17
Pranav Pahuja <20995301+pranavpahuja@users.noreply.github.com>
2025-12-29 02:46:32 +0530

    UI Tweaks

--- js/script.js
+++ js/script.js
@@ -79,4 +79,25 @@
 
 
 
+
+    // Animation Trigger
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('is-visible');
+            observer.unobserve(entry.target); // Only animate once
+        }
+      });
+    }, observerOptions);
+
+    $('.fade-in-section').each(function() {
+        observer.observe(this);
+    });
+
 })(jQuery);
